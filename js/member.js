ede21da9a34e61d4d87ff28ebcc0f61536072d6c
CM-M.emor.Y <43669088+CMemorY180819@users.noreply.github.com>
2019-06-24 11:08:57 +0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -26,5 +26,397 @@ var member = [
   {
     "phone": "No.7",
     "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
+  },
+  {
+    "phone": "No.1",
+    "name": "Hikari"
+  },
+  {
+    "phone": "No.2",
+    "name": "linlinlin"
+  },
+  {
+    "phone": "No.3",
+    "name": "Barret"
+  },
+  {
+    "phone": "No.4",
+    "name": "SandBoat"
+  },
+  {
+    "phone": "No.5",
+    "name": "Indinity"
+  },
+  {
+    "phone": "No.6",
+    "name": "孔乙己的茴香豆"
+  },
+  {
+    "phone": "No.7",
+    "name": "cfh"
   }
 ]
